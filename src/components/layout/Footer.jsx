a913c9f994a6cc6c2515160a4c570a9a625c64a7
ZildMalik <abule443@gmail.com>
2025-05-04 13:51:58 +0800

    index.html update

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -33,7 +33,6 @@ const Footer = () => {
                 <li><a href="https://github.com/" target="_blank" rel="noopener noreferrer">GitHub</a></li>
                 <li><a href="https://linkedin.com/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
                 <li><a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">Twitter</a></li>
-                <li><a href="https://dev.to/" target="_blank" rel="noopener noreferrer">Dev.to</a></li>
               </ul>
             </div>
           </div>
